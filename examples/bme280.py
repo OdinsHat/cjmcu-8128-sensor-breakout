3815adc1cbed0d14f2478e333a78db9ce0139a05
Doug Bromley <doug@tintophat.com>
2018-07-28 13:38:46 +0100

    Adding hdc1080 example script with attached library before factoring it out to its own module

--- examples/bme280.py
+++ examples/bme280.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
-from Adafruit_BME280 import *
+"""Basic BMP280 usage example for the CJMCU 8118  and 8128 breakout boards.
+"""
+from Adafruit_BME280 import BME280_OSAMPLE_8
+from Adafruit_BME280 import BME280
+
 
 def bme280example():
     """The main program function
@@ -7,17 +11,18 @@ def bme280example():
     (temp, pressure, humidity) = getdata()
     printdata(temp, pressure, humidity)
 
+
 def getdata():
-    """The address parameter is important here as most BME/P280 sensors 
+    """The address parameter is important here as most BME/P280 sensors
     occupy the default 0x77 address as noticed in the Adafruit library.
     This can be easily verified by running i2cdetect -y 1 on your Pi
     """
     sensor = BME280(
-            t_mode=BME280_OSAMPLE_8, 
-            p_mode=BME280_OSAMPLE_8, 
-            h_mode=BME280_OSAMPLE_8, 
-            address=0x76
-            )
+        t_mode=BME280_OSAMPLE_8,
+        p_mode=BME280_OSAMPLE_8,
+        h_mode=BME280_OSAMPLE_8,
+        address=0x76
+        )
 
     degrees = sensor.read_temperature()
     pascals = sensor.read_pressure()
@@ -26,10 +31,14 @@ def getdata():
 
     return (degrees, hectopascals, humidity)
 
-def printdata():
+
+def printdata(degrees, hectopascals, humidity):
+    """Print the data provided by the sensor
+    """
     print('Temp      = {0:0.3f} deg C').format(degrees)
     print('Pressure  = {0:0.2f} hPa').format(hectopascals)
     print('Humidity  = {0:0.2f} %').format(humidity)
+
 
 if __name__ == '__main__':
     bme280example()
